6f80ccf0717f87d679bfdd6b11269032d62d74a3
Adrian Pavel <adrian199909@gmail.com>
2022-06-24 14:28:08 +0200

    backend now calculates the number of evaluations

--- backend/server.py
+++ backend/server.py
@@ -7,38 +7,45 @@ ic.progress_bar=True
 
 app = Flask(__name__)
 
-def get_nova_range():
-    white_nova_start = pendulum.datetime(2022, 6, 13);
-    actual_date = pendulum.now();   
 
-    end = white_nova_start.add(days=14);
+def get_nova_range(white_nova_start):
+    actual_date = pendulum.now() 
+
+    end = white_nova_start.add(days=14)
 
     while (actual_date.timestamp() >= end.timestamp()):
         white_nova_start = white_nova_start.add(days=14);
-        end = end.add(days=14);
+        end = end.add(days=14)
 
     return {"start": white_nova_start, "end": end}
 
-def get_time_between_dates(user_login, start_date, end_date) -> dict:
+def get_time_between_dates(user_login, start_date, end_date, white_nova_start) -> dict:
     params = {
-        "range[begin_at]":f"{start_date.to_date_string()},{end_date.to_date_string()}",
+            #"range[begin_at]":f"{start_date.to_date_string()},{end_date.to_date_string()}",
+            "page[size]": 100
     }
-   
+
     locations = ic.pages_threaded(f"/users/{user_login}/locations", params=params)
 
     total = 0 
 
     for location in locations:
 
-      location_begin_at = pendulum.parse(location['begin_at'])
+        location_end_at = pendulum.now()
 
-      location_end_at = pendulum.now()
+        if (location['end_at'] is not None):
+            location_end_at = pendulum.parse(location['end_at'])
+        
+        if (location_end_at.timestamp() < white_nova_start.timestamp()): 
+            continue
 
-      if (location['end_at'] is not None):
-        location_end_at = pendulum.parse(location['end_at'])
+        location_begin_at = pendulum.parse(location['begin_at'])
+        
+        if (location_begin_at.timestamp() < white_nova_start.timestamp()):
+            location_begin_at = white_nova_start
 
-      total = total + (location_end_at.timestamp() - location_begin_at.timestamp())
-    
+        total = total + (location_end_at.timestamp() - location_begin_at.timestamp())
+
     raw_hours = total / 60 / 60
     hours = int(total / 60 / 60)
     minutes = int((raw_hours - hours) * 60) 
@@ -47,26 +54,50 @@ def get_time_between_dates(user_login, start_date, end_date) -> dict:
 
 @app.route('/', methods=["GET"])
 def index(): 
+    white_nova_start = pendulum.datetime(2022, 6, 17)
     options = request.args.to_dict()
     user_login = options.get('login') 
 
-    date_range = get_nova_range();
+    date_range = get_nova_range(white_nova_start);
 
     try:
-        time = get_time_between_dates(user_login, date_range['start'], date_range['end'])
+        time = get_time_between_dates(user_login, date_range['start'], date_range['end'], white_nova_start)
     except:
         return jsonify({"ok": 0, "message": "Error"})
-    
+
     payload = {
-        "hours": time['hours'],
-        "minutes": time['minutes'],
-        "raw_hours": time['raw_hours'],
-        "start": date_range['start'].format("DD/MM/YYYY"),
-        "end": date_range['end'].format("DD/MM/YYYY")
+            "hours": time['hours'],
+            "minutes": time['minutes'],
+            "raw_hours": time['raw_hours'],
+            "start": date_range['start'].format("DD/MM/YYYY"),
+            "end": date_range['end'].format("DD/MM/YYYY"),
+            "evaluations": historic() 
     }
-    
+
     response = jsonify(payload)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-app.run()
+def historic():
+    white_nova_start = pendulum.datetime(2022, 6, 17)
+    options = request.args.to_dict()
+    user_login = options.get('login') 
+
+    date_range = get_nova_range(white_nova_start)
+
+    params = {
+        "sort": "-created_at",
+        "filter[reason]": "Earning after defense",
+        "range[created_at]": f"{date_range['start'].to_date_string()},{date_range['end'].to_date_string()}"
+    }
+
+    try:
+        historic = ic.get(f"/users/{user_login}/correction_point_historics", params=params);
+    except:
+        return jsonify({"ok": 0})
+    
+    return len(historic.json()) 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
