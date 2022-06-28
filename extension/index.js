decebf0944e7ad09b152f2c442a94da9f39de71f
Adrian <adrian199909@gmail.com>
2022-06-28 11:13:44 +0200

    Refactor: switch between white nova and blackhole panels

--- extension/index.js
+++ extension/index.js
@@ -1,87 +1,66 @@
-function swap_display(a, b) {
-
-	let c = a.style.display;
-	a.style.display = b.style.display;
-	b.style.display = c;
-
-}
-
 
 class Switch {
-	
-	constructor(a, a_icon, b, b_icon) {
+    constructor(a_icon, a, b_icon, b) {
+        this.a_ = a;
+        this.a_icon_ = a_icon;
+        this.b_ = b;
+        this.b_icon_ = b_icon;
+        this.addIconsEvents();
+    }
 
-		a_icon.addEventListener("click", this.flip, a, a_icon, b, b_icon);
-		b_icon.addEventListener("click", this.flip, a, a_icon, b, b_icon);
+    addIconsEvents() {
+        this.a_icon_.addEventListener("click", () => {this.switch()});
+        this.b_icon_.addEventListener("click", () => {this.switch()});
+    }
 
-	}
-	
-
-	flip(e, a, a_icon, b, b_icon) {
-		
-		console.log("IN : ", a);
-		swap_display(a, b);
-		swap_display(a_icon,b_icon);
-	}
+    switch() {
+        this.swap_display(this.a_, this.b_);
+        this.swap_display(this.a_icon_, this.b_icon_);
+    }
+    
+    swap_display(a, b) {
+        let c = a.style.display;
+        a.style.display = b.style.display;
+        b.style.display = c;
+    }
 
 }
 
 class ProgressBar {
+    constructor() {
 
-	constructor(element) {
+    }
 
-		this.element_ = element;
-		this.text_ = "";
-		this.progress_ = 0;
-	}
 
-	set_text(text) {
-
-		this.text_ = text;
-		this.element_.querySelector("#progress-bar-text").innerHTML = this.text_;
-	}
-
-	set_progress(progress) {
-
-		this.progress_ = progress;
-		this.element_.querySelector("#progress-bar-nova").style.width = this.progress_ + "%";
-	}
 }
 
-class Application {
-	constructor() {
-		this.init();	
-	}
+class Application {
 
-	async init() {
+    constructor() {
+        this.init();
+    }
 
-		const HTML_file = await fetch(chrome.runtime.getURL("layout.html"));
-		const HTML_text = await HTML_file.text();
-		const goals_container = document.getElementById("goals_container");
+    async init() {
+        const HTML_file = await fetch(chrome.runtime.getURL("layout.html"));
+        const HTML_text = await HTML_file.text();
+        const goals_container = document.getElementById("goals_container");
 
-		goals_container.innerHTML = goals_container.innerHTML + HTML_text;
-		goals_container.style.position = "relative";
+        goals_container.innerHTML = goals_container.innerHTML + HTML_text;
+        goals_container.style.position = "relative";
 
-		this.switch_blackhole = document.getElementById("switch-blackhole");
-		this.blackhole_body = document.getElementById("blackhole-date");
+        this.switch_blackhole = document.getElementById("switch-blackhole");
+        this.blackhole_body = document.getElementById("blackhole-date");
 
-		this.switch_nova = document.getElementById("switch-nova");
-		this.nova_body = document.getElementById("nova-body");
+        this.switch_nova = document.getElementById("switch-nova");
+        this.nova_body = document.getElementById("nova-body");
 
-		this.switch_blackhole.style.display = "none";
-		this.blackhole_body.style.display = "none";
+        this.switch_blackhole.style.display = "none";
+        this.blackhole_body.style.display = "none";
 
+        this.switch = new Switch(this.switch_nova, this.nova_body, this.switch_blackhole, this.blackhole_body);
+    }
 
-		//instance all objects
-		this.switch_button = new Switch(this.blackhole_body, this.switch_blackhole, this.nova_body, this.switch_nova);
-		this.progress_bar = new ProgressBar(document.getElementById("progress-bar-container"));
-
-	//	this.run();
-	}
-
-	run() {
-
-	}
 }
 
-const app = new Application();
+app = new Application();
+
